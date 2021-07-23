88db16ec27629974d189ccb9f9fecc69037f6b96
alexpin <intracomof@gmail.com>
2021-07-23 12:45:57 +0700

    [compatibility] getExtSource

--- src/Model/InstagramFetchedResource.php
+++ src/Model/InstagramFetchedResource.php
@@ -3,4 +3,14 @@ namespace AnyDownloader\InstagramDownloader\Model;
 
 use AnyDownloader\DownloadManager\Model\FetchedResource;
 
-final class InstagramFetchedResource extends FetchedResource { }
+final class InstagramFetchedResource extends FetchedResource
+{
+    /**
+     * @return string
+     */
+    public function getExtSource(): string
+    {
+        return 'instagram';
+    }
+}
+
